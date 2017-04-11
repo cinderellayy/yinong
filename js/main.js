a52840c0fdc54329c100017ec397f90c16390774
yuying <1213513167@qq.com>
2017-04-11 17:37:05 +0800

    生产管理与生产计划

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@
   // 排产管理
   $('.page a').click(function () {
     $(this).addClass('current').siblings().removeClass('current');
+  });
+
+  //排产查看
+  $('.scheduling-view').click(function () {
+     $('.scheduling-view-content').removeClass('hide').addClass('show');
   })
+  $('.close-view').click(function () {
+    $('.scheduling-view-content').removeClass('show').addClass('hide');
+  })
+
+  //排产编辑
+  $('.scheduling-edit').click(function () {
+    $('.scheduling-edit-content').removeClass('hide').addClass('show');
+  })
+  $('.close-edit').click(function () {
+    $('.scheduling-edit-content').removeClass('show').addClass('hide');
+  })
+
+
 })()
