011e7085ccc37fab7445a9596907f5bc1bce8e89
yuying <1213513167@qq.com>
2017-04-12 16:00:44 +0800

    生产计划-编辑

--- js/main.js
+++ js/main.js
@@ -30,4 +30,36 @@
     $(this).addClass('add-active').siblings().removeClass('add-active');
   })
 
+  //排产计划编辑
+  $('.grow-rule').click(function () {
+    $(this).addClass('bg-green').siblings().removeClass('bg-green');
+  })
+  //图表相关
+  var chart = Highcharts.chart('container', {
+    xAxis: {
+       categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        labels:{ 
+        step:2 //刻度间隔
+    } 
+    },
+     yAxis:{  
+      title: {  
+            text: ''  
+        },  
+      tickPixelInterval:10 
+    },  
+    title: {  
+      text: ''//标题为空  
+    },  
+    credits: {  
+        enabled: false//右下角图表版权信息不显示  
+    },
+    legend: {  
+      enabled: false//右下角图表版权信息不显示  
+    },  
+    series: [{
+        data: [12, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 594.1, 95.6, 54.4]
+    }]
+});
+
 })()
